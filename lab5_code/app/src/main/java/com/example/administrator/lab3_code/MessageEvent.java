bb5ebbe181375e62e3b901b918fbaa0ce994b3ff
sillymistake <1074369959@qq.com>
2017-11-07 17:37:30 +0800

    11.7

---
+++ lab5_code/app/src/main/java/com/example/administrator/lab3_code/MessageEvent.java
@@ -0,0 +1,24 @@
+package com.example.administrator.lab3_code;
+
+/**
+ * Created by Administrator on 2017/10/29.
+ */
+
+
+public class MessageEvent {
+
+    public String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
